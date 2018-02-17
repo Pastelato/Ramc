3ea21c17318339229843f417afa18cab76cc0b1e
unknown <sergiomichelottic@gmail.com>
2018-02-16 22:41:25 -0400

    Subida de todos los archivos

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,6 @@
+@extends("master")
+@section("content")
+
+<h1>contacto</h1>
+
+@endsection
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,7 @@
+@extends("master")
+@section("content")
+
+<h1>INICIO</h1>
+<a href="contacto">contacto</a>
+
+@endsection
